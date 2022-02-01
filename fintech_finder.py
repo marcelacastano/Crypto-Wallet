e46b6ac8013df69cc041f9d87bb687c879461588
Grep <mc13c@my.fsu.edu>
2022-01-31 23:17:25 -0500

    sidebar now reads validated transaction receipt

--- fintech_finder.py
+++ fintech_finder.py
@@ -118,7 +118,7 @@ if st.sidebar.button("Send Transaction"):
     transaction_receipt= send_transaction(w3, account, candidate_address, wage)
 
     # Markdown for the transaction hash
-    st.sidebar.markdown("#### Validated Transaction Hash")
+    st.sidebar.markdown("#### Validated Transaction Receipt")
 
     # Write the returned transaction receipt to the screen
     st.sidebar.write(transaction_receipt)
@@ -144,30 +144,16 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 ################################################################################
 # Inspect the Transaction
 
-# Send a test transaction by using the application’s web interface, and then
-# look up the resulting transaction hash in Ganache.
+# Launch the Streamlit application on the integrated terminal with `streamlit run fintech_finder.py`.
 
-# Complete the following steps:
+# On the resulting webpage, select a candidate that you would like to hire from the appropriate drop-down menu. 
+# Then, enter the number of hours that you would like to hire them for.
 
-# 1. From your terminal, navigate to the project folder that contains
-# your `.env` file and the `fintech_finder.py` and `crypto_wallet.py` files.
-# Be sure to activate your Conda `dev` environment if it is not already active.
-
-# 2. To launch the Streamlit application,
-# type `streamlit run fintech_finder.py`.
-
-# 3. On the resulting webpage, select a candidate that you would like to hire
-# from the appropriate drop-down menu. Then, enter the number of hours that you
-# would like to hire them for. (Remember, you do not have a lot of ether in
-# your account, so you cannot hire them for long!)
-
-# 4 Click the Send Transaction button to sign and send the transaction with
-# your Ethereum account information. If the transaction is successfully
-# communicated to Ganache, validated, and added to a block,
-# a resulting transaction hash code will be written to the Streamlit
-# application sidebar.
+# Click the Send Transaction button to sign and send the transaction with your Ethereum account information. 
+# If the transaction is successfully communicated to Ganache, validated, and added to a block,
+# a resulting transaction hash code will be written to the Streamlit application sidebar.
     
-# 5. Navigate to the Ganache accounts tab and locate your account (index 0).
+# Navigate to the Ganache accounts tab and locate your account (index 0).
     # * Take a screenshot of the address, balance, and transaction (TX) count.
     # Save this screenshot to the README.md file of your GitHub repository for
     #  this Challenge assignment.
